18bdfe6288780265036bb692942db91c439934e9
kaho-lc <lc1536328699@163.com>
2023-03-30 20:31:07 +0800

    初始化

---
+++ HelloWorld/Log.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+void Log(const char *message) { std::cout << "Hello World!" << std::endl; }
---
+++ HelloWorld/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+void Log(const char *message);
+
+int main() {
+  Log("hello world!");
+  std ::cin.get();
+}
